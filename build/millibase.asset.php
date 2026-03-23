71219a0b10094f4cfe01902c8ac00d560e120166
Philipp Wellmer <philipp@ouun.io>
2026-03-23 16:49:42 +0100

    feat(ui): Add sticky positioning to tabs for improved navigation experience

--- build/millibase.asset.php
+++ build/millibase.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-sanitize'), 'version' => 'd4da9821158923690110');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-sanitize'), 'version' => '78f07e27142827a4c1b6');
