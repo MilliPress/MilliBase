8a1c0e667cc91ff097b1ee372b0b60229ebf2e8a
Philipp Wellmer <philipp@ouun.io>
2026-04-16 00:39:26 +0300

    chore: rebuild assets

--- build/millibase.asset.php
+++ build/millibase.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-sanitize'), 'version' => '984da898204d6b06d059');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-sanitize'), 'version' => '7e4bd276735ca463979c');
