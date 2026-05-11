2f20b786559b77b25ee52aed0cb26cd06f377f5d
Philipp Wellmer <philipp@ouun.io>
2026-05-11 13:21:03 +0200

    fix(fields): Rename month duration constant to support lowercase requirement

--- build/millibase.asset.php
+++ build/millibase.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-sanitize'), 'version' => '89dac738bf95d266b12c');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-sanitize'), 'version' => 'ed3037524fdea316af28');
