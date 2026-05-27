ed46f256211a904ec508d95005949c7882b732be
Philipp Wellmer <philipp@ouun.io>
2026-05-27 16:21:37 +0200

    chore: Update asset files

--- build/millibase.asset.php
+++ build/millibase.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-sanitize'), 'version' => '96c337d66aa6eec28153');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-sanitize'), 'version' => '54ffeddb50759210055b');
